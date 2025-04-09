75bfa06a5a61fa60313e11803d9c0a11d7e4b0e8
XieZongChen <952937570@qq.com>
2025-04-09 09:49:17 +0800

    feat: 隧道优化

--- src/mesh.js
+++ src/mesh.js
@@ -1,9 +1,14 @@
 import * as THREE from 'three';
 
-const geometry = new THREE.CylinderGeometry(30, 50, 1000);
+/**
+ * 创建模型为圆柱体
+ * 参数为：上底面半径 30、下底面半径 50、高度 1000、圆周分段数 32、纵向分段数 32、是否封闭 true
+ */
+const geometry = new THREE.CylinderGeometry(30, 50, 1000, 32, 32, true);
 
 const material = new THREE.MeshBasicMaterial({
   color: 'orange',
+  side: THREE.DoubleSide, // 双面渲染
 });
 
 const tunnel = new THREE.Mesh(geometry, material);
