def4c001f06b08c1d356238a43c7c7008d024908
XieZongChen <952937570@qq.com>
2025-04-09 10:08:12 +0800

    feat: 实现 uv 动画

--- src/main.js
+++ src/main.js
@@ -17,7 +17,15 @@ camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 
+const clock = new THREE.Clock(); // 创建时钟对象
 function render() {
+  // 获取每次渲染的时间间隔
+  const delta = clock.getDelta();
+
+  // 修改纹理贴图的偏移量，实现 uv 动画
+  mesh.material.map.offset.y += delta * 0.5; // 纹理贴图在 Y 轴方向偏移
+  mesh.rotation.y += delta * 0.5; // 圆柱体绕 Y 轴旋转
+
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
